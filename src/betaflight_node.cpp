c4f413eff4bf958e40f38dccd64d9e111bc16c77
Ivan Lin <i.lin@mail.utoronto.ca>
2024-03-11 02:15:20 -0400

    Implement thrust and body rate conversion. Still need to solve compilation issue.

--- src/betaflight_node.cpp
+++ src/betaflight_node.cpp
@@ -16,7 +16,7 @@
 #include <msp/Client.hpp>
 #include <msp/msp_msg.hpp>
 
-#include <Eigen/Geometry>
+#include <agilib/bridge/thrust_map.hpp>
 
 double deg2rad(const double deg) {
     return deg/180.0 * M_PI;
@@ -30,6 +30,12 @@ class BetaflightNode {
 private:
     ros::NodeHandle nh;
     msp::client::Client *client;
+
+    agi::ThrustMap thrustmap;
+
+    double voltage;
+    double quad_mass;
+    double max_rate;
 
     ros::Publisher imu_pub;
     ros::Publisher battery_pub;
@@ -53,12 +59,13 @@ public:
     void setup() {
         std::string device;
         std::string command_topic;
+        std::string thrust_map_file;
         int baudrate = 1000000;
         if(nh.getParam("device", device)) {
             if(!nh.getParam("baudrate", baudrate)) {
                 ROS_ERROR("Parameter 'baudrate' not set. Using default baudrate of %i", baudrate);
             }
-            ROS_INFO("Connected to FCU at %s", device.c_str());
+            ROS_INFO("Connecting to FCU at %s", device.c_str());
         }
         else {
             ROS_ERROR("Parameter 'device' not set.");
@@ -67,7 +74,24 @@ public:
         if (!nh.getParam("command_topic", command_topic)) {
             ROS_ERROR("Parameter 'command_topic' not set.");
         }
-    
+
+        // load thrust map
+        if (!nh.getParam("thrust_map_file", thrust_map_file)) {
+            ROS_ERROR("Parameter 'thrust_map_file' not set.");
+        } else if (!thrustmap.load(thrust_map_file)) {
+            ROS_ERROR("Failed to load thrust map!.");
+        } else {
+            ROS_INFO("Successfully loaded thrust map.");
+        }
+
+        if (!nh.getParam("quad_mass", quad_mass)) {
+            ROS_ERROR("Parameter 'quad_mass' not set.");
+        }
+
+        if (!nh.getParam("max_rate", max_rate)) {
+            ROS_ERROR("Parameter 'max_rate' not set.");
+        }
+
         client->start(device, uint(baudrate));
 
         imu_pub = nh.advertise<sensor_msgs::Imu>("bf/imu", 1);
@@ -98,6 +122,8 @@ public:
     }
 
     void onBattery(const msp::msg::Analog &analog) {
+        voltage = analog.vbat;
+
         sensor_msgs::BatteryState battery;
         battery.header.stamp = ros::Time::now();
         battery.voltage = analog.vbat;
@@ -108,12 +134,25 @@ public:
 
     void commandCallBack(const agiros_msgs::Command& cmd) {
         msp::msg::SetRawRc rc(msp::FirmwareVariant::BAFL);
-        rc.channels.resize(16, 1500);
-        rc.channels[0] = 1500; //roll
-        rc.channels[1] = 1500; //pitch
-        rc.channels[2] = 1500; //yaw
-        rc.channels[4] = 1500; //throttle
 
+        const double thrust_out = thrustmap.map(cmd.collective_thrust * quad_mass, voltage);
+
+        // Catch mapping error for zero thrust.
+        const double thrust_apply =
+            std::max(cmd.collective_thrust > 0.0 ? thrust_out : 0.0, 0.0);
+
+        // Convert to unsigned int and clamp in valid SBUS range.
+        const int throttle = std::clamp((int)thrust_apply, 1000, 2000);
+
+        const int roll = std::clamp((int)(cmd.bodyrates.x / max_rate * 500 + 1500), 1000, 2000);
+        const int pitch = std::clamp((int)(cmd.bodyrates.y / max_rate * 500 + 1500), 1000, 2000);
+        const int yaw = std::clamp((int)(cmd.bodyrates.z / max_rate * 500 + 1500), 1000, 2000);
+
+        rc.channels.resize(16, 1500);
+        rc.channels[0] = roll; //roll
+        rc.channels[1] = pitch; //pitch
+        rc.channels[2] = yaw; //yaw
+        rc.channels[3] = throttle; //throttle
 
         client->sendMessageNoWait(rc);
 
